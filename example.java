0edbd01459877d0756990c0793649559b0bbf7e0
yuz7_ <yuz9690@gmail.com>
2022-09-10 01:52:14 +0800

    Create example.java

---
+++ example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class exercise {
+    public static void main(String[] args){
+        System.out.println("All About Methods & Arrays :) ");
+
+        System.out.print("Enter 5 values: ");
+        int num = input.nextInt();
+        
+
+    }
+}
