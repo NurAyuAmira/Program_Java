26bb69bb52e3c4fa18e558b6311e5a4ea153f54e
yuz7_ <yuz9690@gmail.com>
2022-09-10 03:02:58 +0800

    Add files via upload

---
+++ LAB 5B/Question 3/src/Sales.java
@@ -0,0 +1,18 @@
+public class Sales{
+     private String Wid;
+
+     public Sales(String sID){
+          Wid = sID;
+     }
+
+     public String getID(){
+         return Wid;
+     }
+
+     public double getSalesAchieve(){
+       
+        return 3000;
+        
+     }
+
+}
